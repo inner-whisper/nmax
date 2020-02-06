f5d4e9f8c0ec81603431b8416fbf38ee920ab4c4
Maxim Rydkin <maks.rydkin@gmail.com>
2020-02-06 20:12:39 +0300

    добавил базовый функционал чтения данных скриптом из STDIN

--- spec/features/nmax_spec.rb
+++ spec/features/nmax_spec.rb
@@ -2,8 +2,11 @@
 
 RSpec.describe 'nmax CLI', type: :aruba do
   before do
-    run_command 'nmax'
+    # example_path = '../../spec/features/files/example_data.txt'
+    # run_command "cat #{example_path} | grep "Hey'"
+    # run_command "cat #{example_path} | nmax 10000"
+    run_command 'testish'
   end
 
-  it { expect(last_command_started).to have_output 'Hello world' }
+  it { expect(last_command_started).to have_output 'Hey, honey!' }
 end

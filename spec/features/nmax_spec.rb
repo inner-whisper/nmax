41fd276f96e5c0752565f76c97292a05027d8772
Maxim Rydkin <maks.rydkin@gmail.com>
2020-02-07 12:13:22 +0300

    переместил тестовый bash-скрипт ближе к тесту

--- spec/features/nmax_spec.rb
+++ spec/features/nmax_spec.rb
@@ -2,10 +2,7 @@
 
 RSpec.describe 'nmax CLI', type: :aruba do
   before do
-    # example_path = '../../spec/features/files/example_data.txt'
-    # run_command "cat #{example_path} | grep "Hey'"
-    # run_command "cat #{example_path} | nmax 10000"
-    run_command 'testish'
+    run_command 'spec/features/files/cli_test'
   end
 
   it { expect(last_command_started).to have_output 'Hey, honey!' }

aea5f4120745d9a0d72f4e59a5d54498f14628a3
Maxim Rydkin <maks.rydkin@gmail.com>
2020-03-31 21:56:45 +0300

    починил rubocop, добавив top-level comment

--- lib/nmax/cli.rb
+++ lib/nmax/cli.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module Nmax
+  # This class is responsible for command line interface logic
   class CLI
     def run
-      # require 'pry'
-      # binding.pry
       text = STDIN.read
       text.scan(/\d+/).join("\n")
     end

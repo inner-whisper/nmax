54fbe1aa660e7df689bfc8860ff81935168f3d62
Maxim Rydkin <maks.rydkin@gmail.com>
2020-04-29 13:19:35 +0300

    добавил алгоритм чтения входящего потока с буферизацией

--- lib/nmax/number_filter.rb
+++ lib/nmax/number_filter.rb
@@ -21,15 +21,47 @@ module Nmax
       # time bash -c 'cat /Users/innerwhisper/Projects/itpard/theater_tickets/log/test.log | bundle exec exe/nmax 1000'
       # io.read.scan(/\d+/) { |w| numbers_from_text << w.to_i }
 
-      io.each_char do |char|
-        if integer?(char)
-          self.current_number_string = current_number_string + char
+      prev_number = nil
+      chunk_number = nil
+
+      io.each(1000) do |chunk|
+        chunk.scan(/\d+/) do |number|
+          # numbers_from_text << number.to_i
+
+          unless chunk_number.nil?
+            number += chunk_number
+            chunk_number = nil
+          end
+
+          unless prev_number.nil?
+            numbers_from_text << prev_number.to_i
+          end
+
+          prev_number = number
+        end
+
+        if integer?(chunk[-1])
+          chunk_number = prev_number
         else
-          add_current_number_to_array
+          numbers_from_text << prev_number.to_i
+          prev_number = nil
+          chunk_number = nil
         end
       end
 
-      add_current_number_to_array
+      unless prev_number.nil?
+        numbers_from_text << prev_number.to_i
+      end
+
+      # io.each_char do |char|
+      #   if integer?(char)
+      #     self.current_number_string = current_number_string + char
+      #   else
+      #     add_current_number_to_array
+      #   end
+      # end
+
+      # add_current_number_to_array
     end
 
     def current_number_string
